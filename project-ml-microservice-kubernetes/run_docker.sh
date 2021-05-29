616a3477523f5d72df48e0f79a3872f53f28b3a2
wardahsana <wardahsana@gmail.com>
2021-05-29 16:37:21 -0500

    enter code run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=demolocal .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it demolocal bash
+
