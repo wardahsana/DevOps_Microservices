3a844a6953f19e367ba4a6e7fddf1a0230a18002
wardahsana <wardahsana@gmail.com>
2021-06-10 18:40:28 -0500

    fixed container name

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=wardahsana/dlocal
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run test_container --image=$dockerpath --port=80
+kubectl run testcontainer --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward test_container 8002:80
+kubectl port-forward testcontainer 8002:80
