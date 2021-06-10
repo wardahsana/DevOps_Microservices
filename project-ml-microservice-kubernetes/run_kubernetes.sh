cc9077fbed8b0c559214a6f5aca36809f15b9667
wardahsana <wardahsana@gmail.com>
2021-06-10 18:38:40 -0500

    added code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=wardahsana/dlocal
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run test_container --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward test_container 8002:80
